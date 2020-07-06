140cc665a7092127f0daab435e5a19fc665e4e36
Francisco Castro <Franciscommcastro@gmail.com>
2020-07-06 20:54:59 -0300

    Cleaning up project

--- posicaomarte.m
+++
@@ -1,7 +0,0 @@
-function tetafin = posicaomarte(tetaini,t)
-
-T_marte = 686.971; %período orbital de Marte
-deltateta = 2*pi/T_marte*t;
-tetafin = tetaini+deltateta; %posição final de Marte
-
-end
--- posicaoterra.m
+++
@@ -1,7 +0,0 @@
-function tetafin = posicaoterra(tetaini,t)
-
-T_terra = 365.26; %período orbital da Terra
-deltateta = 2*pi/T_terra*t;
-tetafin = tetaini+deltateta; %posição final da Terra
-
-end
